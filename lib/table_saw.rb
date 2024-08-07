17fc7986455d975808ed9d9589235068d2363de2
Hamed Asghari <hasghari@gmail.com>
2024-08-07 08:27:50 -0600

    chore: Update Rails 7.2 version

--- lib/table_saw.rb
+++ lib/table_saw.rb
@@ -40,7 +40,7 @@ module TableSaw
 
   # :nocov:
   def self.connection
-    if ActiveRecord.gem_version < Gem::Version.new('7.2.0.beta3')
+    if ActiveRecord.gem_version < Gem::Version.new('7.2.0.rc1')
       connection_pool.connection
     else
       connection_pool.lease_connection
